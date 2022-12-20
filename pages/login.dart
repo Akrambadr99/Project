d3aec67dac16ec3bf25dfc22df570da806cda143
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-20 23:07:24 +0200

    Add files via upload

--- pages/login.dart
+++ pages/login.dart
@@ -6,7 +6,11 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Column(
+        children: [
+          Text("Data")
+        ],
+      ),
     );
   }
 }
