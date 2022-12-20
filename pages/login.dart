3837d9ee3bf48a8597f58cdb7bc5ce286a54156c
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 01:33:15 +0200

    Add files via upload

--- pages/login.dart
+++ pages/login.dart
@@ -7,84 +7,86 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 247, 247, 247),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 64,
-              ),
-
-              TextField(
-                  keyboardType: TextInputType.emailAddress,  // create for is variable
-                  obscureText: false, // Create for is variable
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Email: ",
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context),),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blueGrey,),),
-                    filled: true,
-                    contentPadding: const EdgeInsets.all(8),
-                  ).copyWith(hintText: "Enter your Email : ")),
-
-                 // The SizeBox between Email address and password
-                 const SizedBox(height: 33,),
-
-              TextField(
-                  keyboardType: TextInputType.emailAddress,  // create for is variable
-                  obscureText: false, // Create for is variable
-                  decoration: decorationTextField
-              ),
-
-              const SizedBox(
-                height: 33,
-              ),
-
-              TextField(
-                  keyboardType: TextInputType.text,  // create for is variable
-                  obscureText: true, // Create for is variable
-                  decoration: decorationTextField.copyWith(hintText: "Enter Your Password")
-              ),
-
-              const SizedBox(
-                height: 64,
-              ),
-
-              ElevatedButton(
-                onPressed: (){},
-                child: Text("Sign in", style: TextStyle(fontSize: 19),),  // For sign in
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(BTNgreen),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
-                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
                 ),
-              )
-              ),
 
-              const SizedBox(
-                height: 64,
-              ),
+                TextField(
+                    keyboardType: TextInputType.emailAddress,  // create for is variable
+                    obscureText: false, // Create for is variable
+                    decoration: InputDecoration(
+                      hintText: "Enter Your Email: ",
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: Divider.createBorderSide(context),),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blueGrey,),),
+                      filled: true,
+                      contentPadding: const EdgeInsets.all(8),
+                    ).copyWith(hintText: "Enter your Email : ")),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Do not have an account", style: TextStyle(fontSize:  18 ),),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text("Sign Up" , style: TextStyle(color: Colors.black , fontSize:  18 ),),
+                   // The SizeBox between Email address and password
+                   const SizedBox(height: 33,),
 
-                  )
-                ],
-              )
+                TextField(
+                    keyboardType: TextInputType.emailAddress,  // create for is variable
+                    obscureText: false, // Create for is variable
+                    decoration: decorationTextField
+                ),
+
+                const SizedBox(
+                  height: 33,
+                ),
+
+                TextField(
+                    keyboardType: TextInputType.text,  // create for is variable
+                    obscureText: true, // Create for is variable
+                    decoration: decorationTextField.copyWith(hintText: "Enter Your Password")
+                ),
+
+                const SizedBox(
+                  height: 64,
+                ),
+
+                ElevatedButton(
+                  onPressed: (){},
+                  child: Text("Sign in", style: TextStyle(fontSize: 19),),  // For sign in
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(BTNgreen),
+                    padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                  ),
+                )
+                ),
+
+                const SizedBox(
+                  height: 64,
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Do not have an account", style: TextStyle(fontSize:  18 ),),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text("Sign Up" , style: TextStyle(color: Colors.black , fontSize:  18 ),),
+
+                    )
+                  ],
+                )
 
 
 
-            ],
+              ],
+            ),
           ),
         ),
       ),
