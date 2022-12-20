c64d1ea3599f7009ed7846bcae319e0868103456
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 00:04:21 +0200

    Add files via upload

--- constants/custom_textfield.dart
+++ constants/custom_textfield.dart
@@ -8,16 +8,18 @@ class MyTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return
       TextField(
-        keyboardType: TextInputType.emailAddress,
-        obscureText: false,
+        keyboardType: TextInputType.text,
+        obscureText: true,
         decoration: InputDecoration(
           hintText: "Enter Your Email : ",
 
 // To delete borders
-          enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blueGrey,),),
+          enabledBorder: OutlineInputBorder(
+            borderSide: Divider.createBorderSide(context),),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueGrey,),),
           filled: true,
           contentPadding: const EdgeInsets.all(8),
-        ),
-      ),
-
+        ));
+  }
+  }
