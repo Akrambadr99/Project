d2e38fefbf0ef84172acc7ee4d0f61134a78643f
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-20 23:33:56 +0200

    Add files via upload

--- pages/login.dart
+++ pages/login.dart
@@ -20,19 +20,14 @@ class Login extends StatelessWidget {
                 obscureText: false,
                 decoration: InputDecoration(
                   hintText: "Enter Your Email : ",
-
-
+                  
                   // To delete borders
                   enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
                   focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.blueGrey,),),
                   filled: true,
                   contentPadding: const EdgeInsets.all(8),
-
-
                 ),
               ),
-
-
               Text("Data")
             ],
           ),
