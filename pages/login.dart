6bcece9de80e57d132e2c4783f17b619dec5f040
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-20 23:37:04 +0200

    Add files via upload

--- pages/login.dart
+++ pages/login.dart
@@ -33,7 +33,7 @@ class Login extends StatelessWidget {
 
 
              // The SizeBox between Email address and password
-              const SizedBox(height: 65,),
+              const SizedBox(height: 33,),
 
 
 
