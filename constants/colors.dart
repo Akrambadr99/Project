5455f0266d0156f008e4c5abac1e67e3557417b6
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 01:14:06 +0200

    Add files via upload

--- constants/colors.dart
+++ constants/colors.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+ // This is Colors is used in Project
 const BTNpink = Color.fromARGB(255, 241, 39, 100);
 const BTNgreen = Color.fromARGB(255, 73, 179, 105);
 const appbarGreen = Color.fromARGB(255, 76, 141, 95);
