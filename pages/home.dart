979317548489f3867065d27089c470479b330acf
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 15:53:38 +0200

    Add files via upload

--- pages/home.dart
+++ pages/home.dart
@@ -112,4 +112,9 @@ class Home extends StatelessWidget {
                                 fontSize: 16,
                                 color: Color.fromARGB(255, 0, 0, 0)),
                           ),
-                          
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Color.fromARGB(211, 164, 255, 193),
+                              shape: BoxShape.circle)),
+                    ),
+                    
