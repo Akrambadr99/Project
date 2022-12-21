703a4fbc3ff9e21960ff54b1980756412f7cac6b
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 15:31:08 +0200

    Add files via upload

--- pages/home.dart
+++ pages/home.dart
@@ -1,12 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
-
+import 'package:flower_app/shared/colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+ 
