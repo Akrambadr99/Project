efe66ac18d178423c04898c4754fb0bc0026bc1a
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 15:46:09 +0200

    Add files via upload

--- pages/home.dart
+++ pages/home.dart
@@ -1,6 +1,4 @@
-
-// This is for home Page
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flower_app/shared/colors.dart';
 import 'package:flutter/material.dart';
@@ -11,44 +9,6 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
-        appBar: AppBar(
-          actions: [
-            Row(
-              children: [
-                Stack(
-                  children: [
-                    Positioned(
-                      bottom: 23,
-                      child: Container(
-                          child: Text(
-                            "8",
-                            style: TextStyle(
-                                fontSize: 19, color: Color.fromARGB(255, 0, 0, 0)),
-                          ),
-                          padding: EdgeInsets.all(6),
-                          decoration: BoxDecoration(
-                              color: Color.fromARGB(219, 164, 255, 193),
-                              shape: BoxShape.circle)),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.add_shopping_cart),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 13),
-                  child: Text(
-                    "\$ 13",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ],
-            ),
-          ],
-          backgroundColor: appbarGreen,
-          title: Text("Home"),
-        ));
-  }
-}
+        body: Padding(
+          padding: const EdgeInsets.only(top: 22),
+          }
