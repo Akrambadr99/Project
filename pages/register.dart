4e1f89893d83dfb88995e78deab30d6373b1dc8d
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 14:52:12 +0200

    Add files via upload

--- pages/register.dart
+++ pages/register.dart
@@ -2,3 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:the_project/constants/colors.dart';
 import 'package:the_project/constants/custom_textfield.dart';
 
+class Register extends StatelessWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 64,
+                ),
+
+               
