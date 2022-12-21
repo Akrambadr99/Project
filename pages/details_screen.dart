5a40fe690a6d11e202d1fdb80057b2a22ac612c1
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 16:18:10 +0200

    Add files via upload

--- pages/details_screen.dart
+++ pages/details_screen.dart
@@ -101,4 +101,9 @@ class _DetailsState extends State<Details> {
                         size: 26,
                         color: Color.fromARGB(255, 255, 191, 0),
                       ),
+                      Icon(
+                        Icons.star,
+                        size: 26,
+                        color: Color.fromARGB(255, 255, 191, 0),
+                      ),
                       
