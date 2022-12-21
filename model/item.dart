7a4fd95ca162efdc379842410943cab95aa559d2
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-21 16:23:34 +0200

    Add files via upload

--- model/item.dart
+++ model/item.dart
@@ -7,7 +7,7 @@ class Item {
 
 
 final List<Item> items = [
-  Item(price: 12.99, imgPath: "assets/img/1.webp"),
+  Item(price: 12.99, imgPath: "assets/img/1.webp", location: "Ali shop"),
   Item(price: 12.99, imgPath: "assets/img/2.webp"),
   Item(price: 12.99, imgPath: "assets/img/3.webp"),
   Item(price: 12.99, imgPath: "assets/img/4.webp"),
