7ba0ff82c367de7ed5037b16ce67a95ab7228ea8
Akrambadr99 <111325267+Akrambadr99@users.noreply.github.com>
2022-12-24 00:55:55 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,22 +1,43 @@
-import 'package:flutter/material.dart';
+
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flower_app/pages/checkout.dart';
+import 'package:flower_app/pages/details_screen.dart';
 import 'package:flower_app/pages/home.dart';
-import 'package:the_project/pages/login.dart';
-import 'package:the_project/pages/register.dart';
+import 'package:flower_app/pages/login.dart';
+import 'package:flower_app/pages/register.dart';
+import 'package:flower_app/provider/cart.dart';
+import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const App());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: home(),
+    return     ChangeNotifierProvider(
+      create: (context) {return Cart();},
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
     );
   }
 }
+
+
+
+
+
+
+
